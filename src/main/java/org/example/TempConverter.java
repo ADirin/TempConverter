40d49384c22802c4ed3182eb161d327923718e27
amirdi <amir.dirin@yahoo.com>
2024-09-14 08:20:33 +0300

    Initial Commit fall 2024.

--- src/main/java/org/example/TempConverter.java
+++ src/main/java/org/example/TempConverter.java
@@ -1,5 +1,5 @@
 package org.example;
-public class FahrtToCel {
+public class TempConverter {
 	
 	public static int convertFahr(int f) {
 		int c=  (int)((f-32) * 5.0/9.0);
--- src/test/java/org/example/FahrtToCelTest.java
+++ src/test/java/org/example/FahrtToCelTest.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.Test;
 import static junit.framework.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
-class FahrtToCelTest {
+class TempConverterTest {
  // initial method
     @Test
     void convertFahr() {
-        int test = (FahrtToCel.convertFahr(32));
+        int test = (TempConverter.convertFahr(32));
 
         assertEquals(0, test);
 
@@ -19,20 +19,20 @@ class FahrtToCelTest {
         // Add in the class demo
     @Test
     void convertCel(){
-         int test= (FahrtToCel.convertCel(20));
+         int test= (TempConverter.convertCel(20));
          assertEquals(68,test);
     }
 
     // Inclass exercise
     @Test
     void convertKel(){
-        int test= (FahrtToCel.kelvintoCel(273));
+        int test= (TempConverter.kelvintoCel(273));
         assertEquals(0,test);
     }
 
     @Test
     void convertKeltoFarTest() {
-        double test =Math.round(FahrtToCel.kelvinToFah(300.1));
+        double test =Math.round(TempConverter.kelvinToFah(300.1));
         assertEquals(59.0, test);
     }
     }
